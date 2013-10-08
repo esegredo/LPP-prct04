3c57803397d515c3f2e711bce4b232b77daad94f
Gara Miranda <gmiranda@ull.edu.es>
2013-10-08 16:02:37 +0100

    Incorporada la función check_size_product

--- matrix_operations.rb
+++ matrix_operations.rb
@@ -3,3 +3,10 @@
 # Main program
 a = [[2, 6], [12, 5], [27, -3]]
 b = [[5, -7, 10], [34, 54, -2]]
+
+# Checks if the product can be done with a and b
+def check_size_product(a, b)
+  return false if (a[0].length != b.length)
+  return true
+end
+
