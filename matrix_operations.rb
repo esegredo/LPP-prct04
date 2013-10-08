d954e2def55cf637dd9d1bfe13ad488ec544be1d
Gara Miranda <gmiranda@ull.edu.es>
2013-10-08 16:03:43 +0100

    Incorporada la función product

--- matrix_operations.rb
+++ matrix_operations.rb
@@ -10,3 +10,21 @@ def check_size_product(a, b)
   return true
 end
 
+# Performs the product
+def product(a, b)
+  if check_size_product(a, b)
+    result = Array.new(a.length) { Array.new(b[0].length) }
+    for i in 0...a.length
+      for j in 0...b[0].length
+        sum = 0
+        for k in 0...a[0].length
+          sum += a[i][k] * b[k][j]
+        end
+        result[i][j] = sum
+      end
+    end
+    return result
+  end
+  return false
+end
+
