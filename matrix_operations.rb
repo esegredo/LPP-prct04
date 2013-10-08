9e5826681804425e6603b8512a0596ae6177a042
Eduardo Segredo <edusegre@gmail.com>
2013-10-08 15:58:00 +0100

    Añadida funcion que comprueba las dimensiones de las matrices para su suma

--- matrix_operations.rb
+++ matrix_operations.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/ruby
 
+# Checks if the sum can be done with a and b
+def check_size_sum(a, b)
+  return false if ((a.length != b.length) || (a[0].length != b[0].length))
+  return true
+end
+
 # Main program
 a = [[2, 6], [12, 5], [27, -3]]
 b = [[5, -7, 10], [34, 54, -2]]
