1edd7da370ff1f6e9c4f40b8110ef93f8b49ca82
Eduardo Segredo <edusegre@gmail.com>
2013-10-08 16:00:34 +0100

    Añadida funcion que lleva a cabo la suma de dos matrices

--- matrix_operations.rb
+++ matrix_operations.rb
@@ -6,6 +6,20 @@ def check_size_sum(a, b)
   return true
 end
 
+# Performs the sum
+def sum(a, b)
+  if check_size_sum(a, b)
+    result = Array.new(a.length) { Array.new(a[0].length) }
+    for i in 0...a.length
+      for j in 0...a[0].length
+        result[i][j] = a[i][j] + b[i][j]
+      end
+    end
+    return result
+  end
+  return false
+end
+
 # Main program
 a = [[2, 6], [12, 5], [27, -3]]
 b = [[5, -7, 10], [34, 54, -2]]
